e54ac873b58d6c0ce4ca5871b24e1695160da710
김준형 <kimjunhyeong@gimjunhyeong-ui-MacBook-Air.local>
2018-03-13 19:42:11 +0900

    0312_179p_2-1

---
+++ Test/app/src/main/java/org/techtown/test/MainActivity.java
@@ -0,0 +1,22 @@
+package org.techtown.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void addContentView(View view, ViewGroup.LayoutParams params) {
+        super.addContentView(view, params);
+    }
+}
+
+
