0b8f3ecd1c0c6b8b4b77a55aa01a761dd471796c
markim94 <markim94@naver.com>
2018-04-16 17:41:40 +0900

    0416_p258_2-3

---
+++ SampleLayoutInflater/app/src/main/java/org/techtown/samplelayoutinflater/MenuActivity.java
@@ -0,0 +1,33 @@
+package org.techtown.samplelayoutinflater;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        final LinearLayout container = (LinearLayout) findViewById(R.id.container);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1, container, true);
+
+                CheckBox checkBox = (CheckBox) container.findViewById(R.id.checkBox);
+                checkBox.setText("로딩되었어요");
+            }
+        });
+    }
+}
