602fe6b15a464d5659e45a9cffbfe3e0aa393c82
markim94 <markim94@naver.com>
2018-04-19 18:38:02 +0900

    0419_p325_2-3-8

--- SampleReceiver/app/src/main/java/org/techtown/samplereceiver/MainActivity.java
+++ SampleReceiver/app/src/main/java/org/techtown/samplereceiver/MainActivity.java
@@ -1,7 +1,14 @@
 package org.techtown.samplereceiver;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
+
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "SMS수신권한 있음", Toast.LENGTH_LONG).show();
+        } else{
+            Toast.makeText(this,"SMS수신권한 없음",Toast.LENGTH_LONG).show();
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.RECEIVE_SMS)){
+                Toast.makeText(this,"SMS권한설명 필요함", Toast.LENGTH_LONG).show();
+            } else{
+
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECEIVE_SMS},1);
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:{
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,"SMS 권한을 사용자가 승인함", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this,"SMS 권한 거부", Toast.LENGTH_LONG).show();
+                }
+
+
+                return;
+            }
+        }
     }
 }
