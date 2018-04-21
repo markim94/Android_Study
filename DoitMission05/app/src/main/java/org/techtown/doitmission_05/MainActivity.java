ec0160ebaa8f21a1cdaccd8daa2b6e1aab496844
markim94 <markim94@naver.com>
2018-04-21 23:18:35 +0900

    0421_p331_2-3-9

--- DoitMission05/app/src/main/java/org/techtown/doitmission_05/MainActivity.java
+++ DoitMission05/app/src/main/java/org/techtown/doitmission_05/MainActivity.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_MENU = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +18,22 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = (Button) findViewById(R.id.button);
         TextView textView = (TextView) findViewById(R.id.textview);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),MenuActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_MENU);
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    public void onButton1Clicked(View view){
-        Intent intent = new Intent();
-        startActivityForResult(intent, RESULT_OK);
+        if(resultCode == RESULT_OK){
+            String name = data.getExtras().getString("name");
+            Toast.makeText(getApplicationContext(),"응답으로 전달된 name" + name, Toast.LENGTH_LONG).show();
+        }
     }
 }
--- DoitMission05/app/src/main/java/org/techtown/doitmission_05/MenuActivity.java
+++ DoitMission05/app/src/main/java/org/techtown/doitmission_05/MenuActivity.java
@@ -1,7 +1,10 @@
 package org.techtown.doitmission_05;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("name","menumanage");
+                setResult(RESULT_OK, intent);
+
+                finish();
+            }
+        });
+
     }
 }
