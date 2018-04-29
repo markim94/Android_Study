50add42ce4498deed18f3150c33c85f0db46d089
markim94 <markim94@naver.com>
2018-04-29 23:44:13 +0900

    0429_p346_2-4-1

--- SampleEvent/app/src/main/java/org/techtown/sampleevent/MainActivity.java
+++ SampleEvent/app/src/main/java/org/techtown/sampleevent/MainActivity.java
@@ -2,13 +2,27 @@ package org.techtown.sampleevent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    GestureDetector detector;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == event.KEYCODE_BACK){
+            Toast.makeText(this, "시스템 [BACK] 버튼이 눌렸습니다",Toast.LENGTH_LONG).show();
+
+            return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         view.setOnTouchListener(new View.OnTouchListener(){
             @Override
-            public boolean onTouch(View v, MotionEvent motionEvent) {
+            public boolean onTouch(View view, MotionEvent motionEvent) {
                 int action = motionEvent.getAction();
 
                 float curX = motionEvent.getX();
@@ -40,11 +54,65 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        View view2 = findViewById(R.id.view2);
+        view2.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                detector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
+
+
+        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent){
+                println("onDown() 호출");
+
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+                println("onShowPress() 호출");
+
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                println("onSingleTapUp() 호출");
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println("onScroll() 호출 : " + v + "," + v1);
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+                println("onLongPress() 호출");
+            }
+
+            @Override
+            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println("onFling() 호출 : " + v + "," + v1);
+                return true;
+            }
+
+
+        });
+
     }
 
     public void println(String data){
         textView.append(data + "\n");
     }
+
+
 
 
 }
