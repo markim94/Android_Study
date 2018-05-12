085bc9262c12b9c196abd3fa2c2e8df6b45b7b26
markim94 <markim94@naver.com>
2018-05-12 15:34:03 +0900

    teamcadi_0512

---
+++ Cadi_0512/app/src/main/java/org/techtown/cadi_0512/SecondActivity.java
@@ -0,0 +1,13 @@
+package org.techtown.cadi_0512;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
