7b003bf58cc97f8b62c24e4f6a41bc3abbc20a57
markim94 <markim94@naver.com>
2018-09-24 22:58:30 +0900

    added DrawerLayout project

---
+++ DrawerLayout/app/src/main/java/org/markim/cadi/mydrawerlayout/MainActivity.java
@@ -0,0 +1,48 @@
+package org.markim.cadi.mydrawerlayout;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // 전체화면인 DrawerLayout 객체 참조
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        // Drawer 화면(뷰) 객체 참조
+        final View drawerView = (View) findViewById(R.id.drawer);
+
+
+        // 드로어 화면을 열고 닫을 버튼 객체 참조
+        Button btnOpenDrawer = (Button) findViewById(R.id.btn_OpenDrawer);
+        Button btnCloseDrawer = (Button) findViewById(R.id.btn_CloseDrawer);
+
+
+        // 드로어 여는 버튼 리스너
+        btnOpenDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+
+        // 드로어 닫는 버튼 리스너
+        btnCloseDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(drawerView);
+            }
+        });
+
+        
+    }
+}
